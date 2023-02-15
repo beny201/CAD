4e2a203773207668a1e4ffdd8172cb51068d9f5d
beny201 <artur.wudz@gmail.com>
2023-02-15 21:22:14 +0100

    cleaninig

--- Trash.py
+++ Trash.py
@@ -1,20 +1,15 @@
-def compute_depth(n):
-    i = 1
-    container = []
-    while len(container) <= 9:
-        first = n*i
-        second = list(str(first))
-        for x in second:
-            if x not in container:
-                container.append(x)
-        i += 1
-    return i
+def stray(arr):
+    first = []
+    second = []
+    for x in arr:
+        if x not in first:
+            first.append(x)
+        else:
+            second.append(x)
+    value = [y for y in first if y not in second]
+    return value 
+                
 
-list("42")
+z =[2, 3, 2, 2, 2]
 
-
-print(compute_depth(2))
-
-y = [0,1,2,3,4,5,6,7,8,9]
-
-len(y)
+print(stray(z))
